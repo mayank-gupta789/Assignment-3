7e6d1a02c2adbd2ef6de13eb88410e1e1b4a2f10
mayank-gupta789 <146634330+mayank-gupta789@users.noreply.github.com>
2025-05-12 15:57:39 +0530

    Add files via upload

---
+++ Assignment 3.2.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number =  "))
+
+import math
+c = math.sqrt(n)
+print("Square root =  ",c)
+
+log = math.log(n)
+print("Logarithm =  ",log)
+
+sine = math.sin(n)
+print("Sine =  ",sine)
